4afd21d99a7b8cde42a62de932571a2bc4e7dceb
Nanda Marri <nandamarri@gmail.com>
2025-04-14 07:08:15 +0000

    Initial commit

---
+++ plugins/jira-backend/src/plugin.ts
@@ -0,0 +1,41 @@
+import {
+  coreServices,
+  createBackendPlugin,
+} from '@backstage/backend-plugin-api';
+import { createRouter } from './router';
+import { catalogServiceRef } from '@backstage/plugin-catalog-node/alpha';
+import { createTodoListService } from './services/TodoListService';
+
+/**
+ * jiraPlugin backend plugin
+ *
+ * @public
+ */
+export const jiraPlugin = createBackendPlugin({
+  pluginId: 'jira',
+  register(env) {
+    env.registerInit({
+      deps: {
+        logger: coreServices.logger,
+        auth: coreServices.auth,
+        httpAuth: coreServices.httpAuth,
+        httpRouter: coreServices.httpRouter,
+        catalog: catalogServiceRef,
+      },
+      async init({ logger, auth, httpAuth, httpRouter, catalog }) {
+        const todoListService = await createTodoListService({
+          logger,
+          auth,
+          catalog,
+        });
+
+        httpRouter.use(
+          await createRouter({
+            httpAuth,
+            todoListService,
+          }),
+        );
+      },
+    });
+  },
+});
